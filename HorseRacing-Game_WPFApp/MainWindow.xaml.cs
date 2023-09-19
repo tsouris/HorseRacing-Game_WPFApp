6e85efa448fd4b767cafab7b32ecb15b3c366992
tsouris <nikolastsouris16@gmail.com>
2023-09-19 17:31:34 +0300

    Added one piece bg

--- HorseRacing-Game_WPFApp/MainWindow.xaml.cs
+++ HorseRacing-Game_WPFApp/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace HorseRacing_Game_WPFApp
 {
@@ -10,6 +10,18 @@ namespace HorseRacing_Game_WPFApp
         public MainWindow()
         {
             InitializeComponent();
+
+            Line1.Visibility = Visibility.Collapsed;
+            Line2.Visibility = Visibility.Collapsed;
+            Line3.Visibility = Visibility.Collapsed;
+            Line4.Visibility = Visibility.Collapsed;
+            Line5.Visibility = Visibility.Collapsed;
+
+            Button1.Visibility = Visibility.Collapsed;
+            Button2.Visibility = Visibility.Collapsed;
+            Button3.Visibility = Visibility.Collapsed;
+            Button4.Visibility = Visibility.Collapsed;
+            Button5.Visibility = Visibility.Collapsed;
         }
 
         private async void StartButton_Click(object sender, RoutedEventArgs e)
@@ -29,6 +41,20 @@ namespace HorseRacing_Game_WPFApp
             CountdownPopup.IsOpen = true;
             await Task.Delay(1000);
             CountdownPopup.IsOpen = false;
+
+            Line1.Visibility = Visibility.Visible;
+            Line2.Visibility = Visibility.Visible;
+            Line3.Visibility = Visibility.Visible;
+            Line4.Visibility = Visibility.Visible;
+            Line5.Visibility = Visibility.Visible;
+
+            Button1.Visibility = Visibility.Visible;
+            Button2.Visibility = Visibility.Visible;
+            Button3.Visibility = Visibility.Visible;
+            Button4.Visibility = Visibility.Visible;
+            Button5.Visibility = Visibility.Visible;
+
+            StartButton.IsEnabled = false;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
