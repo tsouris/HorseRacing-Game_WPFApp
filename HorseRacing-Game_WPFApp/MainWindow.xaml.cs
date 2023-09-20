71dd8002d9770346c2022885732bf33db484e79b
tsouris <nikolastsouris16@gmail.com>
2023-09-20 19:36:59 +0300

    Added logic for button click and added new assets

--- HorseRacing-Game_WPFApp/MainWindow.xaml.cs
+++ HorseRacing-Game_WPFApp/MainWindow.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace HorseRacing_Game_WPFApp
 {
     public partial class MainWindow : Window
     {
         private bool raceStarted = false;
+
+        private int luffyPosition = 0;
+        private int zoroPosition = 0;
+        private int namiPosition = 0;
+        private int ussopPosition = 0;
+        private int sanjiPosition = 0;
+        private int lineEndPosition = 542;
         public MainWindow()
         {
             InitializeComponent();
@@ -22,6 +29,12 @@ namespace HorseRacing_Game_WPFApp
             Button3.Visibility = Visibility.Collapsed;
             Button4.Visibility = Visibility.Collapsed;
             Button5.Visibility = Visibility.Collapsed;
+
+            Luffy.Visibility = Visibility.Collapsed;
+            Zoro.Visibility = Visibility.Collapsed;
+            Nami.Visibility = Visibility.Collapsed;
+            Ussop.Visibility = Visibility.Collapsed;
+            Sanji.Visibility = Visibility.Collapsed;
         }
 
         private async void StartButton_Click(object sender, RoutedEventArgs e)
@@ -54,12 +67,72 @@ namespace HorseRacing_Game_WPFApp
             Button4.Visibility = Visibility.Visible;
             Button5.Visibility = Visibility.Visible;
 
+            Luffy.Visibility = Visibility.Visible;
+            Zoro.Visibility = Visibility.Visible;
+            Nami.Visibility = Visibility.Visible;
+            Ussop.Visibility = Visibility.Visible;
+            Sanji.Visibility = Visibility.Visible;
+
             StartButton.IsEnabled = false;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (sender == Button1)
+            {
+                luffyPosition += 10;
+                TranslateTransform transform = new TranslateTransform(luffyPosition, 0);
+                Luffy.RenderTransform = transform;
+
+                if (luffyPosition >= lineEndPosition)
+                {
+                    MessageBox.Show("Luffy is the winner!");
+                }
+            }
+            else if(sender == Button2) 
+            {
+                zoroPosition += 10;
+                TranslateTransform transform = new TranslateTransform(zoroPosition, 0);
+                Zoro.RenderTransform = transform;
+
+                if(zoroPosition >= lineEndPosition) 
+                {
+                    MessageBox.Show("Zoro is the winner");
+                }
+            }
+            else if(sender == Button3)
+            {
+                namiPosition += 10;
+                TranslateTransform transform = new TranslateTransform(namiPosition, 0);
+                Nami.RenderTransform = transform;
+
+                if(namiPosition >= lineEndPosition)
+                {
+                    MessageBox.Show("Nami is the winner");
+                }
+            }
+            else if( sender == Button4)
+            {
+                ussopPosition += 10;
+                TranslateTransform transform = new TranslateTransform(ussopPosition, 0);
+                Ussop.RenderTransform = transform;
+
+                if(ussopPosition >= lineEndPosition)
+                {
+                    MessageBox.Show("Ussop is the winner");
+                }
+            }
+            else if (sender == Button5) 
+            {
+                sanjiPosition += 10;
+                TranslateTransform transform = new TranslateTransform(sanjiPosition, 0);
+                Sanji.RenderTransform = transform;
+
+                if(sanjiPosition >= lineEndPosition)
+                {
+                    MessageBox.Show("Sanji is the winner");
+                }
+            }
         }
     }
 }
